3dde9a3a86dd7202336cd4318e41196f0d7dabbf
Dave <orangechannel@pm.me>
2019-12-23 04:52:35 -0500

    Day 02: finish golfing

--- Day 02/day02_golf.py
+++ Day 02/day02_golf.py
@@ -1,19 +1,19 @@
 """
-376 chars, 15 lines, 516ms
-small error on L19: if part 2 noun,verb was (12,2) again, won't print
+342 chars, 12 lines, 494ms
+things wrong with this solution:
+- won't print part 2 if same values as (12,2) from part 1
+- doesn't check for incorrect optcodes, nor incorrect length of list
+- hugely inefficient brute force method with no break once solution is found
 """
 def i(l):
- k=0
- while x:=len(l):
+ for k in range(0,len(l),4):
   if l[k]==99:break
   if l[k]==1:l[l[k+3]]=l[l[k+1]]+l[l[k+2]]
   elif l[k]==2:l[l[k+3]]=l[l[k+1]]*l[l[k+2]]
-  else:quit()
-  k=k+4if k<=x-5else x-1
  return l[0]
 from itertools import *
 for o,p in product(range(100),repeat=2):
  n=[int(_)for _ in list(open('2'))[0].split(',')]
- n[1:3],g=[o,p],19690720
+ n[1:3]=[o,p]
  if(o,p)==(12,2):print(i(n))
- elif i(n)==g:print(100*o+p)
+ elif i(n)==19690720:print(100*o+p)
