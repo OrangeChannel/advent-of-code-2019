90275a9dc76678896adddd24ba9e7455cb58232f
Dave <orangechannel@pm.me>
2019-12-23 02:05:16 -0500

    Day 01: continue golfing

--- Day 01/day01_golf_lines.py
+++ Day 01/day01_golf_lines.py
@@ -1,4 +1,3 @@
-"""145 chars, 3 lines, 0 imports, 16.5 ms"""
-f=[i for i in open('1')]
-print(sum([(c:=lambda m:int(m)//3-2)(i)for i in f]))
-print(sum([(p:=lambda m:0if c(m)<=0else c(m)+p(c(m)))(i)for i in f]))
+"""161 chars, 2 lines, 0 imports, 16.4 ms"""
+print(sum([(c:=lambda m:int(m)//3-2)(i)for i in[i for i in open('1')]]))
+print(sum([(p:=lambda m:0if c(m)<=0else c(m)+p(c(m)))(i)for i in[i for i in open('1')]]))
