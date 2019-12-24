33100f5ee36fdfb58c4798bc8d53a4d0aad864e1
Dave <orangechannel@pm.me>
2019-12-23 23:10:55 -0500

    Day 03: clean up + Day 01: compress lines

--- Day 01/day01_golf_lines.py
+++ Day 01/day01_golf_lines.py
@@ -1,3 +1,2 @@
-"""161 chars, 2 lines, 0 imports, 16.4 ms"""
-print(sum([(c:=lambda m:int(m)//3-2)(i)for i in[i for i in open('1')]]))
-print(sum([(p:=lambda m:c(m)+p(c(m))if c(m)>0else 0)(i)for i in[i for i in open('1')]]))
+"""154 chars, 1 lines, 0 imports, 16.4 ms"""
+print(sum([(c:=lambda m:int(m)//3-2)(i)for i in[i for i in open('1')]]),sum([(p:=lambda m:c(m)+p(c(m))if c(m)>0else 0)(i)for i in[i for i in open('1')]]))
--- Day 03/day03.py
+++ Day 03/day03.py
@@ -1,29 +1,27 @@
+""" 157 ms """
 awire = open('day03_input.txt').readlines()[0].rstrip().split(',')
 bwire = open('day03_input.txt').readlines()[1].rstrip().split(',')
-# awire = ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72']
-# bwire = ['U62', 'R66', 'U55', 'R34', 'D71', 'R55', 'D58', 'R83']
-# awire = ['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51']
-# bwire = ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7']
-
 dx = {'L': -1, 'R': 1, 'U': 0, 'D': 0}
 dy = {'L': 0, 'R': 0, 'D': -1, 'U': 1}
 
 
 def points(l: list):
     pointlist = {}  # had to get a hint on this one, lists were too slow for part 2
-    x, y, t = 0, 0, 0
+    x, y, acc = 0, 0, 0
     for _ in l:
-        for i in range(int(_[1:])):
-            x += dx[_[0]]
-            y += dy[_[0]]
-            t += 1
+        d = _[0]
+        n = int(_[1:])
+        for i in range(1, n + 1):
+            x += dx[d]
+            y += dy[d]
             if (x, y) not in pointlist:  # only include the distance for the first time the wire hits point (x,y)
-                pointlist[(x, y)] = t
+                pointlist[(x, y)] = acc + i
+        acc += n  # avoid increasing length for each move, 7% performance increase with this particular input
     return pointlist
 
 
 apoints = points(awire)
 bpoints = points(bwire)
-common = set(apoints.keys()).intersection(set(bpoints.keys()))  # much faster than looping over lists to check for common points
+common = set(apoints.keys()) & set(bpoints.keys())
 print('Part 1: {}'.format(min([abs(x) + abs(y) for (x, y) in common])))
 print('Part 2: {}'.format(min([apoints[i] + bpoints[i] for i in common])))
