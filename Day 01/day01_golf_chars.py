7c51cddc077176bba1e16c9c11620cb4a4dcaec4
Dave <orangechannel@pm.me>
2019-12-23 21:41:28 -0500

    Day 01: lol

--- Day 01/day01_golf_chars.py
+++ Day 01/day01_golf_chars.py
@@ -1,7 +1,7 @@
-"""137 chars, 6 lines, 16.5 ms"""
+"""136 chars, 6 lines, 16.5 ms"""
 f=open('1')
 c=lambda m:int(m)//3-2
-p=lambda m:0if c(m)<=0else c(m)+p(c(m))
+p=lambda m:c(m)+p(c(m))if c(m)>0else 0
 print(sum([c(i)for i in f]))
 f.seek(0)
 print(sum([p(i)for i in f]))
--- Day 01/day01_golf_lines.py
+++ Day 01/day01_golf_lines.py
@@ -1,3 +1,3 @@
 """161 chars, 2 lines, 0 imports, 16.4 ms"""
 print(sum([(c:=lambda m:int(m)//3-2)(i)for i in[i for i in open('1')]]))
-print(sum([(p:=lambda m:0if c(m)<=0else c(m)+p(c(m)))(i)for i in[i for i in open('1')]]))
+print(sum([(p:=lambda m:c(m)+p(c(m))if c(m)>0else 0)(i)for i in[i for i in open('1')]]))
