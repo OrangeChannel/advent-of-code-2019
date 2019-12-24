43af8999f7c1a848269f0627b8c75b79a65e97c9
Dave <orangechannel@pm.me>
2019-12-23 21:45:45 -0500

    Day 03: lambda to trim 17 more chars

--- Day 03/day03_golf.py
+++ Day 03/day03_golf.py
@@ -1,7 +1,7 @@
 """
-441 chars, 11 lines
+424 chars, 11 lines
 """
-a,b,c,d=open('3').readlines()[0].rstrip().split(','),open('3').readlines()[1].rstrip().split(','),{'L':-1,'R':1,'U':0,'D':0},{'L':0,'R':0,'D':-1,'U':1}
+a,b,c,d,h=open('3').readlines()[0].rstrip().split(','),open('3').readlines()[1].rstrip().split(','),{'L':-1,'R':1,'U':0,'D':0},{'L':0,'R':0,'D':-1,'U':1},lambda x,y:set(x.keys())&set(y.keys())
 def f(l):
  p,x,y,t={},0,0,0
  for _ in l:
@@ -11,4 +11,4 @@ def f(l):
    t+=1
    if(x,y)not in p:p[(x,y)]=t
  return p
-print(min([abs(x)+abs(y)for(x,y)in set(f(a).keys())&set(f(b).keys())]),min([f(a)[i]+f(b)[i]for i in set(f(a).keys())&set(f(b).keys())]))
+print(min([abs(x)+abs(y)for(x,y)in h(f(a),f(b))]),min([f(a)[i]+f(b)[i]for i in h(f(a),f(b))]))
