71567a5bdbdf7cb0e6e5828887e293d5f924d81b
Dave <orangechannel@pm.me>
2019-12-23 23:39:30 -0500

    Clean up

--- Day 01/day01_golf_lines.py
+++ Day 01/day01_golf_lines.py
@@ -1,2 +1,2 @@
-"""154 chars, 1 lines, 0 imports, 16.4 ms"""
-print(sum([(c:=lambda m:int(m)//3-2)(i)for i in[i for i in open('1')]]),sum([(p:=lambda m:c(m)+p(c(m))if c(m)>0else 0)(i)for i in[i for i in open('1')]]))
+"""175 chars, 1 lines, 0 imports, 16.4 ms"""
+print(sum([(c := lambda m: int(m) // 3 - 2)(i) for i in [i for i in open('1')]]), sum([(p := lambda m: c(m) + p(c(m)) if c(m) > 0 else 0)(i) for i in [i for i in open('1')]]))
--- Day 02/day02_golf.py
+++ Day 02/day02_golf.py
@@ -1,19 +1,19 @@
 """
-341 chars, 12 lines, 494ms
+430 chars, 12 lines, 494ms
 things wrong with this solution:
 - won't print part 2 if same values as (12,2) from part 1
 - doesn't check for incorrect optcodes, nor incorrect length of list
 - hugely inefficient brute force method with no break once solution is found
 """
+from itertools import *
 def i(l):
- for k in range(0,len(l),4):
-  if l[k]==99:break
-  if l[k]==1:l[l[k+3]]=l[l[k+1]]+l[l[k+2]]
-  elif l[k]==2:l[l[k+3]]=l[l[k+1]]*l[l[k+2]]
- return l[0]
-from itertools import*
-for o,p in product(range(100),repeat=2):
- n=[int(_)for _ in list(open('2'))[0].split(',')]
- n[1:3]=[o,p]
- if(o,p)==(12,2):print(i(n))
- elif i(n)==19690720:print(100*o+p)
+    for k in range(0, len(l), 4):
+        if l[k] == 99: break
+        if l[k] == 1: l[l[k + 3]] = l[l[k + 1]] + l[l[k + 2]]
+        elif l[k] == 2: l[l[k + 3]] = l[l[k + 1]] * l[l[k + 2]]
+    return l[0]
+for o, p in product(range(100), repeat=2):
+    n = [int(_) for _ in list(open('2'))[0].split(',')]
+    n[1:3] = [o, p]
+    if (o, p) == (12, 2): print(i(n))
+    elif i(n) == 19690720: print(100 * o + p)
