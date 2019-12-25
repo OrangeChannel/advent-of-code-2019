d3e393877ec71448d6d68898ea82864e04820cba
Dave <orangechannel@pm.me>
2019-12-24 21:17:55 -0500

    Day 05: Finish golfing for now

--- Day 05/day05_golf.py
+++ Day 05/day05_golf.py
@@ -1,22 +1,24 @@
-instruction_listb = (instruction_list := [int(instruction) for instruction in open('input.txt').readline().rstrip().split(',')])[:]
-def intcode(input_val, ilist: list):
+for (v, l) in [(1, [int(i) for i in open('input.txt').readline().rstrip().split(',')])[:], (5, [int(i) for i in open('input.txt').readline().rstrip().split(',')])[:]]:
     k = 0
-    def decipher(s: int):
-        return {'opcode': int(str(s)[-2:]), 'modes': [int(i) for i in str(s)[:-2].zfill(5)[::-1][:2] + '0']}
-    def val_mode(mode: int, val: int):
-        return ilist[val] if mode == 0 else val
     while True:
-        if decipher(ilist[k])['opcode'] == 99: break
-        elif decipher(ilist[k])['opcode'] == 1: ilist[ilist[k + 3]] = val_mode(decipher(ilist[k])['modes'][0], ilist[k + 1]) + val_mode(decipher(ilist[k])['modes'][1], ilist[k + 2])
-        elif decipher(ilist[k])['opcode'] == 2: ilist[ilist[k + 3]] = val_mode(decipher(ilist[k])['modes'][0], ilist[k + 1]) * val_mode(decipher(ilist[k])['modes'][1], ilist[k + 2])
-        elif decipher(ilist[k])['opcode'] == 3: ilist[ilist[k + 1]] = input_val
-        elif decipher(ilist[k])['opcode'] == 4: print(val_mode(decipher(ilist[k])['modes'][0], ilist[k + 1]))
-        elif decipher(ilist[k])['opcode'] == 5: k = val_mode(decipher(ilist[k])['modes'][1], ilist[k + 2]) if val_mode(decipher(ilist[k])['modes'][0], ilist[k + 1]) else k + 3
-        elif decipher(ilist[k])['opcode'] == 6: k = val_mode(decipher(ilist[k])['modes'][1], ilist[k + 2]) if not val_mode(decipher(ilist[k])['modes'][0], ilist[k + 1]) else k + 3
-        elif decipher(ilist[k])['opcode'] == 7: ilist[ilist[k + 3]] = 1 if val_mode(decipher(ilist[k])['modes'][0], ilist[k + 1]) < val_mode(decipher(ilist[k])['modes'][1], ilist[k + 2]) else 0
-        elif decipher(ilist[k])['opcode'] == 8: ilist[ilist[k + 3]] = 1 if val_mode(decipher(ilist[k])['modes'][0], ilist[k + 1]) == val_mode(decipher(ilist[k])['modes'][1], ilist[k + 2]) else 0
-        if decipher(ilist[k])['opcode'] in [1, 2, 7, 8]: k += 4
-        elif decipher(ilist[k])['opcode'] in [3, 4]: k += 2
-    return ilist
-intcode(1, instruction_list)
-intcode(5, instruction_listb)
+        if (a:=int(str(l[k])[-2:])) == 99: break
+        elif a == 1: l[l[k + 3]] = (l[l[k + 1]] if (o:=[int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'])[0] == 0 else l[k + 1]) + (l[l[k + 2]] if o[1] == 0 else l[k + 2])
+        elif a == 2: l[l[k + 3]] = (l[l[k + 1]] if (o:=[int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'])[0] == 0 else l[k + 1]) * (l[l[k + 2]] if o[1] == 0 else l[k + 2])
+        elif a == 3: l[l[k + 1]] = v
+        elif a == 4: print(l[l[k + 1]] if [int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else l[k + 1])
+        elif a == 5: k = (l[l[k + 2]] if [int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'][1] == 0 else l[k + 2]) if (l[l[k + 1]] if [int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else l[k + 1]) else k + 3
+        elif a == 6: k = (l[l[k + 2]] if [int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'][1] == 0 else l[k + 2]) if not (l[l[k + 1]] if [int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else l[k + 1]) else k + 3
+        elif a == 7: l[l[k + 3]] = 1 if (l[l[k + 1]] if (o:=[int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'])[0] == 0 else l[k + 1]) < (l[l[k + 2]] if o[1] == 0 else l[k + 2]) else 0
+        elif a == 8: l[l[k + 3]] = 1 if (l[l[k + 1]] if (o:=[int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'])[0] == 0 else l[k + 1]) == (l[l[k + 2]] if o[1] == 0 else l[k + 2]) else 0
+        k = k + 4 if a in [1,2,7,8] else (k + 2 if a in [3,4] else k)
+
+# For learning purposes, taken from https://www.reddit.com/user/u794575248/
+I = open('input.txt').readline().rstrip()
+def R(I,X,e=__import__('operator'),S=list.__setitem__):
+    def J(t,a):nonlocal i;i=a if t else i
+    W=0,4,4,2,2,3,3,4,4;T=lambda f:lambda a,b,c:S(p,c,f(a,b));i,p=0,[*map(int,I.split(','))]
+    F=0,T(e.add),T(e.mul),lambda a:S(p,a,X),print,J,lambda a,b:J(a==0,b),T(e.lt),T(e.eq)
+    D=lambda v:(c:=v%10,w:=W[c],[v//10**j&1|(398&2**c>0)&(j==w)for j in range(2,w+1)])
+    while p[i]!=99:t=i;c,w,M=D(p[i]);F[c](*[x if m else p[x]for m,x in zip(M,p[i+1:])]);J(t==i,t+w)
+R(I,1),R(I,5)
+
--- Day 05/day05_golfv2.py
+++ Day 05/day05_golfv2.py
@@ -1,13 +1,24 @@
-for (input_val, ilist) in [(1, [int(instruction) for instruction in open('input.txt').readline().rstrip().split(',')])[:], (5, [int(instruction) for instruction in open('input.txt').readline().rstrip().split(',')])[:]]:
+for (v, l) in [(1, [int(i) for i in open('input.txt').readline().rstrip().split(',')])[:], (5, [int(i) for i in open('input.txt').readline().rstrip().split(',')])[:]]:
     k = 0
     while True:
-        if (a:=int(str(ilist[k])[-2:])) == 99: break
-        elif a == 1: ilist[ilist[k + 3]] = (ilist[ilist[k + 1]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else ilist[k + 1]) + (ilist[ilist[k + 2]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][1] == 0 else ilist[k + 2])
-        elif a == 2: ilist[ilist[k + 3]] = (ilist[ilist[k + 1]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else ilist[k + 1]) * (ilist[ilist[k + 2]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][1] == 0 else ilist[k + 2])
-        elif a == 3: ilist[ilist[k + 1]] = input_val
-        elif a == 4: print(ilist[ilist[k + 1]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else ilist[k + 1])
-        elif a == 5: k = (ilist[ilist[k + 2]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][1] == 0 else ilist[k + 2]) if (ilist[ilist[k + 1]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else ilist[k + 1]) else k + 3
-        elif a == 6: k = (ilist[ilist[k + 2]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][1] == 0 else ilist[k + 2]) if not (ilist[ilist[k + 1]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else ilist[k + 1]) else k + 3
-        elif a == 7: ilist[ilist[k + 3]] = 1 if (ilist[ilist[k + 1]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else ilist[k + 1]) < (ilist[ilist[k + 2]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][1] == 0 else ilist[k + 2]) else 0
-        elif a == 8: ilist[ilist[k + 3]] = 1 if (ilist[ilist[k + 1]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else ilist[k + 1]) == (ilist[ilist[k + 2]] if [int(i) for i in str(ilist[k])[:-2].zfill(5)[::-1][:2] + '0'][1] == 0 else ilist[k + 2]) else 0
+        if (a:=int(str(l[k])[-2:])) == 99: break
+        elif a == 1: l[l[k + 3]] = (l[l[k + 1]] if (o:=[int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'])[0] == 0 else l[k + 1]) + (l[l[k + 2]] if o[1] == 0 else l[k + 2])
+        elif a == 2: l[l[k + 3]] = (l[l[k + 1]] if (o:=[int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'])[0] == 0 else l[k + 1]) * (l[l[k + 2]] if o[1] == 0 else l[k + 2])
+        elif a == 3: l[l[k + 1]] = v
+        elif a == 4: print(l[l[k + 1]] if [int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else l[k + 1])
+        elif a == 5: k = (l[l[k + 2]] if [int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'][1] == 0 else l[k + 2]) if (l[l[k + 1]] if [int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else l[k + 1]) else k + 3
+        elif a == 6: k = (l[l[k + 2]] if [int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'][1] == 0 else l[k + 2]) if not (l[l[k + 1]] if [int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'][0] == 0 else l[k + 1]) else k + 3
+        elif a == 7: l[l[k + 3]] = 1 if (l[l[k + 1]] if (o:=[int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'])[0] == 0 else l[k + 1]) < (l[l[k + 2]] if o[1] == 0 else l[k + 2]) else 0
+        elif a == 8: l[l[k + 3]] = 1 if (l[l[k + 1]] if (o:=[int(i) for i in str(l[k])[:-2].zfill(5)[::-1][:2] + '0'])[0] == 0 else l[k + 1]) == (l[l[k + 2]] if o[1] == 0 else l[k + 2]) else 0
         k = k + 4 if a in [1,2,7,8] else (k + 2 if a in [3,4] else k)
+
+# For learning purposes, taken from https://www.reddit.com/user/u794575248/
+I = open('input.txt').readline().rstrip()
+def R(I,X,e=__import__('operator'),S=list.__setitem__):
+    def J(t,a):nonlocal i;i=a if t else i
+    W=0,4,4,2,2,3,3,4,4;T=lambda f:lambda a,b,c:S(p,c,f(a,b));i,p=0,[*map(int,I.split(','))]
+    F=0,T(e.add),T(e.mul),lambda a:S(p,a,X),print,J,lambda a,b:J(a==0,b),T(e.lt),T(e.eq)
+    D=lambda v:(c:=v%10,w:=W[c],[v//10**j&1|(398&2**c>0)&(j==w)for j in range(2,w+1)])
+    while p[i]!=99:t=i;c,w,M=D(p[i]);F[c](*[x if m else p[x]for m,x in zip(M,p[i+1:])]);J(t==i,t+w)
+R(I,1),R(I,5)
+
