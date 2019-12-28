d944bf8432f4c4b0d6026fd69bf0646ea3a18671
Dave <orangechannel@pm.me>
2019-12-28 15:18:35 -0500

    Optimize Day 06
    By using same Dict for both parts, and by combining the function logic we get
    an 11% speedup.

--- Day 06/day06.py
+++ Day 06/day06.py
@@ -1,39 +1,42 @@
 l = [line.rstrip().split(')') for line in open('input.txt')]
-centers, orbiters = [i[0] for i in l], [i[1] for i in l]
+centers, orbiters, parents = [i[0] for i in l], [i[1] for i in l], {}
 
 # ----------------------------------- Part 1 --------------------------------- #
-children, parents = {}, {}
+for k, v in enumerate(orbiters): parents[v] = centers[k]
 
-for k, v in enumerate(orbiters):
-    children.setdefault(centers[k], []).append(v)
-    parents[v] = centers[k]
+def travel(pset, ckey, sset=None, d=1):
+    if sset is not None:  # part 1
+        if pset[ckey] in pset.keys():
+            sset[pset[ckey]] = d - 1
+            return travel(pset, pset[ckey], sset, d + 1)
+        else: return sset
+    if pset[ckey] in pset.keys(): return travel(pset, pset[ckey], d=d + 1)
+    else: return d
 
-def paths(d, clist, t=0):
-    for c in clist: t = t + len(d[c]) + paths(d, d[c]) if c in d.keys() else t
-    return t
-
-sub_orbits = sum(paths(children, v) for v in children.values())
-print('Part 1: {}'.format(sub_orbits + paths(children, ['COM'])))
+print('Part 1: {}'.format(sum(travel(parents, v) for v in parents.keys())))
 
 # ----------------------------------- Part 2 --------------------------------- #
-def travel(pset, ckey, sset=None, dist=0):
-    if sset is None: sset = {}
+a, b = travel(parents, 'YOU', {}), travel(parents, 'SAN', {})
 
-    if pset[ckey] in pset.keys():
-        sset[pset[ckey]] = dist
-        return travel(pset, pset[ckey], sset, dist + 1)
-    else: return sset
-
-a, b = travel(parents, 'YOU'), travel(parents, 'SAN')
 print('Part 2: {}'.format(min([a[i] + b[i] for i in (a.keys() & b.keys())])))
 
 # ----------------------------------- Output --------------------------------- #
 # Part 1: 314247
 # Part 2: 514
 
-# Time (mean ± σ):      97.8 ms ±   0.7 ms    [User: 94.3 ms, System: 3.2 ms]
-# Range (min … max):    96.6 ms …  99.5 ms    30 runs
+# Time (mean ± σ):      86.9 ms ±   1.2 ms    [User: 84.3 ms, System: 2.6 ms]
+# Range (min … max):    85.3 ms …  90.5 ms    33 runs
 
 
 # ----------------------------------- Notes ---------------------------------- #
-# travel() doesn't reach 'COM' because 'COM' has no parents
+# def paths(d, clist, t=0):
+#   for c in clist: t = t + len(d[c]) + paths(d, d[c]) if c in d.keys() else t
+#   return t
+
+# def count(pset, ckey, dist=1):
+#   if pset[ckey] in pset.keys(): return count(pset, pset[ckey], dist + 1)
+#   else: return dist
+
+# original algorithms for part 1+2
+# part 1 used a `children` dict instead of the `parents` dict used by part 2
+# ~11% speedup by combining both to use same dict
