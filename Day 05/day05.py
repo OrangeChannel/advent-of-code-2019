c223b6d7f20b9924a045bd8d40c859b9afdb2615
Dave <orangechannel@pm.me>
2019-12-28 19:40:10 -0500

    Golf + Optimize Day 05

--- Day 05/day05.py
+++ Day 05/day05.py
@@ -4,44 +4,34 @@ instruction_listb = (instruction_list := [*map(int, string_list)]).copy()
 # ----------------------------------- Part 1 --------------------------------- #
 def intcode(input_val, ilist, k=0):
     get = lambda mode, val: val if mode else ilist[val]
+    modes = lambda i: [*map(int, str(i)[:-2].zfill(2)[::-1][:2])]
 
-    def decipher(s: int):
-        opcode = int(str(s)[-2:])
-        param_string = str(s)[:-2].zfill(5)[::-1]
-
-        return {'code': opcode, 'modes': [*map(int, param_string[:2])]}
-
-    while (code := decipher(s := ilist[k])['code']) != 99:
-
+    while (code := (s := ilist[k]) % 100) != 99:
         if code == 1:  # add
-            ilist[ilist[k + 3]] = get(decipher(s)['modes'][0], ilist[k + 1]) + \
-                                  get(decipher(s)['modes'][1], ilist[k + 2])
+            ilist[ilist[k + 3]] = get(modes(s)[0], ilist[k + 1]) + \
+                                  get(modes(s)[1], ilist[k + 2])
         elif code == 2:  # multiply
-            ilist[ilist[k + 3]] = get(decipher(s)['modes'][0], ilist[k + 1]) * \
-                                  get(decipher(s)['modes'][1], ilist[k + 2])
+            ilist[ilist[k + 3]] = get(modes(s)[0], ilist[k + 1]) * \
+                                  get(modes(s)[1], ilist[k + 2])
         elif code == 3:  # input
             ilist[ilist[k + 1]] = input_val
         elif code == 4:  # output
-            print(get(decipher(s)['modes'][0], ilist[k + 1]))
+            print(get(modes(s)[0], ilist[k + 1]))
         elif code == 5:  # jump if True
-            k = get(decipher(s)['modes'][1], ilist[k + 2]) if \
-                get(decipher(s)['modes'][0], ilist[k + 1]) else k + 3
+            k = get(modes(s)[1], ilist[k + 2]) if \
+                get(modes(s)[0], ilist[k + 1]) else k + 3
         elif code == 6:  # jump if False
-            k = get(decipher(s)['modes'][1], ilist[k + 2]) if not \
-                get(decipher(s)['modes'][0], ilist[k + 1]) else k + 3
+            k = get(modes(s)[1], ilist[k + 2]) if not \
+                get(modes(s)[0], ilist[k + 1]) else k + 3
         elif code == 7:  # less than
-            ilist[ilist[k + 3]] = 1 if \
-                get(decipher(s)['modes'][0], ilist[k + 1]) < \
-                get(decipher(s)['modes'][1], ilist[k + 2]) else 0
+            ilist[ilist[k + 3]] = 1 if get(modes(s)[0], ilist[k + 1]) < \
+                                       get(modes(s)[1], ilist[k + 2]) else 0
         elif code == 8:  # equals
-            ilist[ilist[k + 3]] = 1 if \
-                get(decipher(s)['modes'][0], ilist[k + 1]) == \
-                get(decipher(s)['modes'][1], ilist[k + 2]) else 0
+            ilist[ilist[k + 3]] = 1 if get(modes(s)[0], ilist[k + 1]) == \
+                                       get(modes(s)[1], ilist[k + 2]) else 0
 
         if code in [1, 2, 7, 8]: k += 4
         elif code in [3, 4]: k += 2
-
-    return ilist
 
 print('Part 1:')
 intcode(1, instruction_list)
@@ -66,5 +56,5 @@ intcode(5, instruction_listb)
 # Part 2:
 # 3892695
 
-# Time (mean ± σ):      17.1 ms ±   0.1 ms    [User: 14.5 ms, System: 2.7 ms]
-# Range (min … max):    16.8 ms …  17.5 ms    156 runs
+# Time (mean ± σ):      16.4 ms ±   0.2 ms    [User: 13.7 ms, System: 2.8 ms]
+# Range (min … max):    16.2 ms …  17.3 ms    157 runs
