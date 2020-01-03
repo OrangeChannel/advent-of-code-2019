57bfcd8a96ebc8384735252498869def6f6cd18a
Dave <orangechannel@pm.me>
2020-01-03 18:51:11 -0500

    Redo Day 08

--- Day 08/day08.py
+++ Day 08/day08.py
@@ -1,69 +1,40 @@
-import more_itertools
+from more_itertools import grouper
+from itertools import product
+data, length, height = open('input.txt').readline().rstrip(), 25, 6
 
-data = open('input.txt').readline().rstrip()
-length, height = 25, 6
-layer_size = length * height
+# ----------------------------------- Part 1 --------------------------------- #
+slicer = grouper(data, (size := length * height))
+(layers := [(i, next(slicer)) for i in range(len(data) // size)]).sort(key=lambda l: l[1].count('0'))
 
-slicer = more_itertools.grouper(data, layer_size)
-
-layers = {}
-for i in range((num_layers := int(len(data)/layer_size))):
-    layers[i] = next(slicer)
-
-num_zeroes = {}
-for i in range(num_layers):
-    num_zeroes[i] = layers[i].count('0')
-
-min_zeroes = min(num_zeroes.values())
-for key in num_zeroes.keys():
-    if min_zeroes == num_zeroes[key]:
-        goal = key
-
-print('Part 1: {}'.format(layers[goal].count('1') * layers[goal].count('2')))
+print('Part 1: {}'.format((g := layers[0][1]).count('1') * g.count('2')))
 
 # ----------------------------------- Part 2 --------------------------------- #
+picture, curr = [list(l[1]) for l in sorted(layers)], 0
 
-picture = []
-for k,v in enumerate(layers[(curr := 0)]):
-    picture.append(v)
-
-while '2' in picture:
-    for k,v in enumerate(picture):
-        if v == '2':
-            picture[k] = layers[curr + 1][k]
+while '2' in (out := picture[0] if curr == 0 else out):  # only defined first time
+    for k, v in enumerate(out):
+        if v == '2': out[k] = picture[curr + 1][k]
     curr += 1
 
-rows = []
-rowcutter = more_itertools.grouper(picture, length)
-for i in range(height):
-    rows.append(next(rowcutter))
+print('Part 2:\n' + u'\u2588' * ((w := 2) * length + 2))
 
-print('Part 2:\n' + u'\u2588' * (2 * length + 2))
-
-for r in range(height):
-    t = u'\u2587'
-    for v in range(length):
-        t += ''.join(rows[r][v]) + ''.join(rows[r][v])
-    t = t.replace('0', u'\u2587')
-    t = t.replace('1', ' ') + u'\u2587'
-    print(t)
-
-print(u'\u2588' * (2 * length + 2))
+for r, c in product(range(height), range(length)):
+    if c == 0: print(u'\u2588' * w, end='')
+    print(' ' * w if out[r * length + c] == '1' else u'\u2587' * w, end='')
+    if c == length - 1: print('')
+    if r == height - 1 and c == length - 1: print(u'\u2588' * (w * length + 2))
 
 # ----------------------------------- Output --------------------------------- #
 # Part 1: 1950
 # Part 2:
 # ████████████████████████████████████████████████████
-# ▇        ▇▇  ▇▇▇▇  ▇▇▇▇    ▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇▇
-# ▇  ▇▇▇▇▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇▇
-# ▇      ▇▇▇▇    ▇▇▇▇▇▇  ▇▇▇▇  ▇▇        ▇▇  ▇▇▇▇▇▇▇▇▇
-# ▇  ▇▇▇▇▇▇▇▇  ▇▇  ▇▇▇▇        ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇▇
-# ▇  ▇▇▇▇▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇▇
-# ▇  ▇▇▇▇▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇        ▇▇▇
+# ██        ▇▇  ▇▇▇▇  ▇▇▇▇    ▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇
+# ██  ▇▇▇▇▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇
+# ██      ▇▇▇▇    ▇▇▇▇▇▇  ▇▇▇▇  ▇▇        ▇▇  ▇▇▇▇▇▇▇▇
+# ██  ▇▇▇▇▇▇▇▇  ▇▇  ▇▇▇▇        ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇
+# ██  ▇▇▇▇▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇
+# ██  ▇▇▇▇▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇        ▇▇
 # ████████████████████████████████████████████████████
 
-# Time (mean ± σ):      20.9 ms ±   0.3 ms    [User: 17.5 ms, System: 3.3 ms]
-# Range (min … max):    20.3 ms …  21.6 ms    124 runs
-
-# ----------------------------------- Notes ---------------------------------- #
-# This is horribly inefficient, ugly, and dumb but it works ;)
+# Time (mean ± σ):      20.5 ms ±   0.1 ms    [User: 17.3 ms, System: 3.3 ms]
+# Range (min … max):    20.2 ms …  20.9 ms    131 runs
