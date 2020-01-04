921e4f4b975f0bddab0f122ad052f770329ea9a0
Dave <orangechannel@pm.me>
2020-01-03 19:51:03 -0500

    Day 08: replace itertools.product with normal nested loop

--- Day 08/day08.py
+++ Day 08/day08.py
@@ -1,5 +1,4 @@
 from more_itertools import grouper
-from itertools import product
 data, length, height = open('input.txt').readline().rstrip(), 25, 6
 
 # ----------------------------------- Part 1 --------------------------------- #
@@ -18,22 +17,23 @@ while '2' in (out := picture[0] if curr == 0 else out):  # only defined first ti
 
 print('Part 2:\n' + u'\u2588' * ((w := 2) * length + 2))
 
-for r, c in product(range(height), range(length)):
-    if c == 0: print(u'\u2588' * w, end='')
-    print(' ' * w if out[r * length + c] == '1' else u'\u2587' * w, end='')
-    if c == length - 1: print('')
-    if r == height - 1 and c == length - 1: print(u'\u2588' * (w * length + 2))
+for r in range(height):
+    print(u'\u2588' * w, end='')
+    for c in range(length):
+        print(' ' * w if out[r * length + c] == '1' else u'\u2588' * w, end='')
+    print('')
+print(u'\u2588' * (w * length + 2))
 
 # ----------------------------------- Output --------------------------------- #
 # Part 1: 1950
 # Part 2:
 # ████████████████████████████████████████████████████
-# ██        ▇▇  ▇▇▇▇  ▇▇▇▇    ▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇
-# ██  ▇▇▇▇▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇
-# ██      ▇▇▇▇    ▇▇▇▇▇▇  ▇▇▇▇  ▇▇        ▇▇  ▇▇▇▇▇▇▇▇
-# ██  ▇▇▇▇▇▇▇▇  ▇▇  ▇▇▇▇        ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇
-# ██  ▇▇▇▇▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇▇▇▇▇
-# ██  ▇▇▇▇▇▇▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇  ▇▇▇▇  ▇▇        ▇▇
+# ██        ██  ████  ████    ████  ████  ██  ████████
+# ██  ████████  ██  ████  ████  ██  ████  ██  ████████
+# ██      ████    ██████  ████  ██        ██  ████████
+# ██  ████████  ██  ████        ██  ████  ██  ████████
+# ██  ████████  ██  ████  ████  ██  ████  ██  ████████
+# ██  ████████  ████  ██  ████  ██  ████  ██        ██
 # ████████████████████████████████████████████████████
 
 # Time (mean ± σ):      20.5 ms ±   0.1 ms    [User: 17.3 ms, System: 3.3 ms]
